1d87fba1403949f000a271f56b9059e26c9ae713
Tetsuya HAMAMOTO <t.hamamoto@suvt.co.jp>
2015-01-02 19:33:22 +0900

    Add: AFxW v1.5.7

---
+++ AFxW/tools/ChocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿try {
+  $package = 'AFxW'
+  $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+  $url = 'http://www.h5.dion.ne.jp/~akt/lzh/afxw32_157.zip'
+  $url64bit = 'http://www.h5.dion.ne.jp/~akt/lzh/afxw64_157.zip'
+  Install-ChocolateyZipPackage $package -url $url -url64bit $url64bit -unzipLocation $destination
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
