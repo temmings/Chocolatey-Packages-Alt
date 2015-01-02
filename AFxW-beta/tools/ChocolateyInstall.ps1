5c0faf0e62f1529f9ac9a6d27e4f089b5da71b1a
Tetsuya HAMAMOTO <t.hamamoto@suvt.co.jp>
2015-01-02 19:40:33 +0900

    Add: AFxW-beta v1.5.8b3

---
+++ AFxW-beta/tools/ChocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿try {
+  $package = 'AFxW-beta'
+  $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+  # base package
+  $url = 'http://www.h5.dion.ne.jp/~akt/lzh/afxw32_157.zip'
+  $url64bit = 'http://www.h5.dion.ne.jp/~akt/lzh/afxw64_157.zip'
+  Install-ChocolateyZipPackage $package -url $url -url64bit $url64bit -unzipLocation $destination
+
+  # overwrite files
+  $url = 'http://www.h5.dion.ne.jp/~akt/lzh/afxw32_158b3.zip'
+  $url64bit = 'http://www.h5.dion.ne.jp/~akt/lzh/afxw64_158b3.zip'
+  Install-ChocolateyZipPackage $package -url $url -url64bit $url64bit -unzipLocation $destination
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
